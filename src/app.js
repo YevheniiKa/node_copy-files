6af764f88834ac2c6d6679ef8fa5ad8a568acc87
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-01-13 14:11:03 +0200

    added solution to the task

--- src/app.js
+++ src/app.js
@@ -1,1 +1,38 @@
+/* eslint-disable no-console */
 'use strict';
+
+const fs = require('fs');
+const path = process.argv[2];
+const copyPath = process.argv[3];
+
+if (!path || !copyPath) {
+  console.error('Error: Please provide source and destination paths');
+}
+
+if (path === copyPath) {
+  console.error('Error: Source and destination paths are the same');
+}
+
+try {
+  const stats = fs.statSync(path);
+
+  if (!stats.isFile()) {
+    console.error('Error: Source path is not a file');
+  }
+
+  if (fs.existsSync(copyPath)) {
+    const destStats = fs.statSync(copyPath);
+
+    if (destStats.isDirectory()) {
+      console.error('Error: Destination path is a directory');
+    }
+  }
+
+  const data = fs.readFileSync(path);
+
+  fs.writeFileSync(copyPath, data);
+
+  console.log(`File copied from "${path}" to "${copyPath}" successfully`);
+} catch (err) {
+  console.error(`Error: ${err}`);
+}
